6c29678d83337bd8b87196688829584872d30adb
ilmechaJu <pawor006@naver.com>
2024-07-22 15:33:57 +0900

    solved JAVA29.java

---
+++ solutions/JAVA29.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long solution(int price, long money, int count) {
+        long answer = 0;
+        for (int i=1; i<=count; i++){
+            money -= price*i;
+        }
+        if (money <0){
+            answer = -money;
+        }
+        else{
+            answer = 0;
+        }
+        return answer;
+    }
+}
+
