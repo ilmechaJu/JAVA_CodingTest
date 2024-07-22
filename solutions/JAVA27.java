1a36958597fae995b072bcdafd2a21eeb3924a27
ilmechaJu <pawor006@naver.com>
2024-07-22 13:49:05 +0900

    solved JAVA27.java

--- solutions/JAVA27.java
+++ solutions/JAVA27.java
@@ -1,3 +1,21 @@
-SELECT ANIMAL_TYPE, IFNULL(NAME, 'No name') AS NAME, SEX_UPON_INTAKE
-FROM ANIMAL_INS
-ORDER BY ANIMAL_ID;
+class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+        for(int i=left; i<=right; i++){
+            int cnt = 0;
+            for (int j =1; j<=i; j++){
+                if (i%j==0){
+                    cnt++;
+                }
+            }
+            if (cnt %2 ==0){
+                answer +=i;
+            }
+            else{
+                answer -=i;
+            }
+        }
+        return answer;
+    }
+}
+
